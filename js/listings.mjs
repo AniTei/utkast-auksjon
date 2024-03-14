6d930cfe035e2504a62dda6166d606c16e00a0cb
AniTei <115176414+AniTei@users.noreply.github.com>
2024-03-14 13:39:57 +0100

    adds limit to listings displayed, also adds const value2, not great revisit

--- js/listings.mjs
+++ js/listings.mjs
@@ -1,17 +1,24 @@
 // get the objects
 // display them, how many? 12?
-// be able to open them in diff page?
+// be able to open them in diff page
 
-//can element be selected and filled? yes
 const listingsContainer = document.querySelector(".listings-container");
 listingsContainer.innerHTML = `<p>hei</p>`;
 
-//can I get the url form other js file? jes
 import { baseURL } from "./endpoints.mjs";
 import { allListings } from "./endpoints.mjs";
 import { getData } from "./getData.mjs";
-const url = baseURL + allListings;
 
+const numberOfPosts = "12";
+
+const limit = "?limit=" + numberOfPosts;
+
+console.log ("limit:" , limit)
+const url = baseURL + allListings + limit;
+
+console.log("url:" , url)
+
+// https://docs.noroff.dev/docs/v2/pagination-sorting
 
 
 /* JavaScript passes a value from a function back to the code that called it 
@@ -21,47 +28,49 @@ or a calculation where the result of the calculation is returned. */
 
 // https://stackoverflow.com/questions/49938266/how-to-return-values-from-async-functions-using-async-await-from-function
 
-// this one is helpful!!
+// this one is helpful
 // https://www.youtube.com/watch?v=4t8Q_YLpuKw
 
 async function displayData() {
   try {
     const value = await getData(url);
 
+    const value2 = value.data;
+
+    console.log("dispayData function, value2", value2)
+
     listingsContainer.innerHTML = "";
 
     //loop throug data, and display, find way wo/ innerHTML
-    for (let i = 0; i < value.data.length; i++) {
+    for (let i = 0; i < value2.length; i++) {
 
       listingsContainer.innerHTML += `<div
         class="card m-3 card col-12 col-sm-6 col-lg-4"
         style="width: 18rem">
         <img
-          src="${value.data[i].media[0].url}"
+          src="${value2[i].media[0].url}"
           class="card-img-top"
-          alt="${value.data[i].media[0].alt}"
+          alt="${value2[i].media[0].alt}"
         />
         <div class="card-body">
-          <h5 class="card-title">${value.data[i].title}</h5>
-          <p class="card-text"> ${value.data[i].description}
+          <h5 class="card-title">${value2[i].title}</h5>
+          <p class="card-text"> ${value2[i].description}
           </p>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">Bids: ${value.data[i]._count.bids}</li>
-          <li class="list-group-item">Deadline: ${value.data[i].endsAt}</li>
+          <li class="list-group-item">Bids: ${value2[i]._count.bids}</li>
+          <li class="list-group-item">Deadline: ${value2[i].endsAt}</li>
         </ul>
         <div class="card-body">
-          <a href="html/listing.html?id=${value.data[i].id}" class="card-link">open</a>
-
-          
+          <a href="html/listing.html?id=${value2[i].id}" class="card-link">open</a>
         </div>
       </div>`;
-
     }
   } catch {}
 }
 
 displayData();
+
 
 
 
