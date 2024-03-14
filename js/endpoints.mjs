0ac297d6f637cf37f33e74819f82d3b55f428c01
AniTei <115176414+AniTei@users.noreply.github.com>
2024-03-14 11:02:10 +0100

    endpoints

---
+++ js/endpoints.mjs
@@ -0,0 +1,19 @@
+export const baseURL = "https://v2.api.noroff.dev";
+
+export const allListings = "/auction/listings/"
+
+export const id = "/<id>"
+
+// id has to be built with the querystinrg 
+
+"https://v2.api.noroff.dev/auction/listings/<id>"
+
+// feks
+// https://v2.api.noroff.dev/auction/listings/e9b647ce-96ee-4fb8-96f5-d543d5be83f9
+
+
+
+/* console.log (url);
+ */
+
+
