aece18a51b69eb2c036b51631fd6a67028477a3a
AniTei <115176414+AniTei@users.noreply.github.com>
2024-03-14 11:00:52 +0100

    search functionality

---
+++ js/search.js
@@ -0,0 +1,101 @@
+//listings on the landingpage are searchable
+
+/* 
+GET
+/auction/listings/search?q=<query>
+Search for listings by their title or description properties. */
+
+import { baseURL } from "./endpoints.mjs";
+import { allListings } from "./endpoints.mjs";
+import { getData } from "./getData.mjs";
+const url = baseURL + allListings;
+
+const input = document.querySelector("input");
+const SearchedListingsContainer = document.querySelector(".searched-listings-container");
+
+SearchedListingsContainer.innerHTML = `<p>hei</p>`;
+
+input.onkeyup = function harvestValue(event) {
+  event.preventDefault();
+
+  console.log("input:", input.value);
+
+  const criterium = input.value;
+  console.log("criterium:", criterium);
+
+  filterListings(criterium);
+};
+
+async function filterListings(kriterium) {
+
+  console.log("Kriterium:", kriterium);
+
+  const listings = await getData(url);
+
+  console.log("why cant this be filtered?:", listings.data);
+
+  // make new array from data gotten
+  const filteredListings = listings.data.filter(function (listing) {
+    if (listing.title.startsWith(kriterium)) {
+      return true;
+    }
+  });
+
+  console.log("filteredListings:", filteredListings);
+
+  displayData(filteredListings);
+}
+
+filterListings();
+
+async function displayData(value) {
+  try {
+    console.log("filteredListings in dispay function:", value);
+    SearchedListingsContainer.innerHTML = "";
+
+    //loop throug data, and display, find way wo/ innerHTML
+    for (let i = 0; i < value.length; i++) {
+      SearchedListingsContainer.innerHTML += `<div
+          class="card m-3 card col-12 col-sm-6 col-lg-4"
+          style="width: 18rem">
+          <img
+            src="${value[i].media[0].url}"
+            class="card-img-top"
+            alt="${value[i].media[0].alt}"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${value[i].title}</h5>
+            <p class="card-text"> ${value[i].description}
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Bids: ${value[i]._count.bids}</li>
+            <li class="list-group-item">Deadline: ${value[i].endsAt}</li>
+          </ul>
+          <div class="card-body">
+            <a href="html/listing.html?id=${value[i].id}" class="card-link">open</a>
+          </div>
+        </div>`;
+    }
+  } catch {}
+}
+
+/* 
+
+   //get the data
+   getData(url);
+   // filter it
+   method?
+   //display it
+    displayData() 
+
+// bruke dataene fra get data, filter the array, criterium: input in search
+
+// se på hva du gjorde i js2 ca
+
+/* get data, filter it on criterium */
+
+// https://content.noroff.dev/javascript-1/single-api-results.html
+
+// konklusjon onsdag kveld: jeg kan filtrere objectene som dukker opp,
+// men hvordan får jeg det til å gjelde alle objectene i array
