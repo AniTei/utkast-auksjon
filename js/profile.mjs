6ffa288de81c306649572dfcdca4fa45f634330b
AniTei <115176414+AniTei@users.noreply.github.com>
2024-03-14 13:34:56 +0100

    adds comment about displaying userlistings on profile

--- js/profile.mjs
+++ js/profile.mjs
@@ -1,2 +1,13 @@
 // how to use query parameters, first put the param, then set it to true, to include. bc boolean
 // https://v2.api.noroff.dev/auction/listings?_seller=true
+
+/* All listings
+
+This endpoint does not require authentication.
+GET
+/auction/listings
+Retrieve all listings.
+
+If you want to get all listings by a specific profile, you can use the listings by profile endpoint.
+
+ */
